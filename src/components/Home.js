37b6140cef9b4e2885fceb9615edb8c4bb2d2e1b
Ruochen Li <liruochenv@gmail.com>
2021-04-21 23:08:50 +0800

    structure of homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const Home = () => (
-  <div>
-    Home Component
-  </div>
-);
 
-Home.propTypes = {};
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: null
+    }
+  }
 
-Home.defaultProps = {};
 
-export default Home;
+  render() {
+    let user = this.state.user
+    let viewComponent = <h1>Loading...</h1>
+    if (user == "volunteer") viewComponent = <Volunteer/>
+    if (user == "donor") viewComponent = <Donor/>
+
+    return(
+      <div>
+        <h1>This is home page</h1>
+        { viewComponent }
+      </div>
+
+    )
+  }
+} 
+
+class Donor extends React.Component {
+  // include dashboard, submit
+  render() {
+    return(
+      <h1>This is donor's home page</h1>
+    )
+  }
+}
+
+class Volunteer extends React.Component {
+  // include dasboard, start page, submit page
+  render() {
+    return(
+      <h1>This is volunteer's home page</h1>
+    )
+  }
+}
+
+class Dashboard extends React.Component {
+  render() {
+    return(
+      <h2> This is the start of the dashboard</h2>
+    )
+  }
+}
